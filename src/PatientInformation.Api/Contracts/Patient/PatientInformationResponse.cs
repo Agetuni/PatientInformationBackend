805a8d43a13c29456290fcee369aefc880e87315
Aleazar yilma <yilma4060@gmail.com>
2023-01-24 18:44:02 +0300

    add id on patiemt response

--- src/PatientInformation.Api/Contracts/Patient/PatientInformationResponse.cs
+++ src/PatientInformation.Api/Contracts/Patient/PatientInformationResponse.cs
@@ -2,11 +2,13 @@
 {
     public class PatientInformationResponse
     {
+        public long Id { get; set; }
         public string FullName { get; private set; }
         public long DiseaseId { get; private set; }//forign key 
         public Epilepsy Epilepsy { get; private set; }
         public List<PatientAllergyResponse> PatientAllergies { get; set; }
         public List<PatientNCDResponse> PatientNCDs { get; set; }
+        public DiseaseResponse Disease { get; set; }
         public PatientInformationResponse()
         {
             PatientAllergies = new List<PatientAllergyResponse>();
